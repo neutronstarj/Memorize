8663e4f23968088e4eaa6485f76ed4c9e57fd49c
Iris He <jiale14@ualberta.ca>
2024-03-22 12:28:07 -0600

    add toggle

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -1,24 +1,39 @@
 import SwiftUI
 struct ContentView: View{
-    var body: Text{
-        Text("hello")
+    var body: some View{
+        HStack{
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
+        }
+        .foregroundColor(.orange)
+        .padding()
     }
 }
 
 struct CardView: View{
-    var isFaceUo : Bool = false
+    @State var isFaceUp = false //you must specfiy this false value, you cannot having a var without value
+    
     var body : some View{
-        ZStack(content:{
-            if isFaceUo{
-                RoundedRectangle(cornerRadius: 12).foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 2)
+        ZStack{
+            //let means constant
+            let base = RoundedRectangle(cornerRadius: 12)
+            if isFaceUp{
+               base.foregroundColor(.white)
+               base.strokeBorder(lineWidth:2)
                 Text("😅").font(.largeTitle)
             }else{
-                RoundedRectangle(cornerRadius: 12)
+                base.fill()
             }
-        })
+        }.onTapGesture  {
+            print("tapped")
+            isFaceUp.toggle()
+            
+        }
     }
 }
+//views are immutable, isFaceuP, if you wanna change, make it @State
 
 struct Previews_ContentView_Previews: PreviewProvider {
     static var previews: some View {
